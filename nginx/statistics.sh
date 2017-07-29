897546eda33748ff8c11116c092373568ac524b6
User Stdusr <stdusr@datahaven.local>
2017-07-29 11:20:27 +0200

    Statistics rounded, check if non-cached stuff is logged

--- nginx/statistics.sh
+++ nginx/statistics.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-HITS=$(grep -e REVALIDATED -e HIT /var/log/nginx/access.log | awk '{print $5}' | awk '{ sum += $1 } END { print sum/(1024^3) }')
-MISSES=$(grep -e MISS /var/log/nginx/access.log | awk '{print $10}' | awk '{ sum += $1 } END { print sum/(1024^3) }')
-NOCACHE=$(awk '{ sum += $9 } END { print sum/(1024^3) }'  /var/log/nginx/access.log-nocache)
+HITS=$(grep -e REVALIDATED -e HIT /var/log/nginx/access.log | awk '{print $10}' | awk '{ sum += $1 } END { printf "%.3f",  sum/(1024^3) }')
+MISSES=$(grep -e MISS /var/log/nginx/access.log | awk '{print $10}' | awk '{ sum += $1 } END { printf "%.3f", sum/(1024^3) }')
+
+if [ -e /var/log/nginx/access.log-nocache ]
+then
+	NOCACHE=$(awk '{ sum += $9 } END { printf "%.3f", sum/(1024^3) }'  /var/log/nginx/access.log-nocache)
+fi
 
 echo "MISS: $MISSES GiB"
 echo "HIT/REVALIDATED: $HITS GiB"
-echo "Requests not matching cache rules: $NOCACHE GiB"
+if [ -n "$NOCACHE" ]
+then
+	echo "Requests not matching cache rules: $NOCACHE GiB"
+fi
