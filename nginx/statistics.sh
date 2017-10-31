a433c1d1068d946343c48eceb369afc2174e4d4d
Charlie Root <root@datahaven.local>
2017-10-31 12:58:16 +0100

    Count EXPIRED as MISS

--- nginx/statistics.sh
+++ nginx/statistics.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 HITS=$(grep -e REVALIDATED -e HIT /var/log/nginx/access.log | awk '{print $10}' | awk '{ sum += $1 } END { printf "%.3f",  sum/(1024^3) }')
-MISSES=$(grep -e MISS /var/log/nginx/access.log | awk '{print $10}' | awk '{ sum += $1 } END { printf "%.3f", sum/(1024^3) }')
+MISSES=$(grep -e MISS -e EXPIRED /var/log/nginx/access.log | awk '{print $10}' | awk '{ sum += $1 } END { printf "%.3f", sum/(1024^3) }')
 
 if [ -e /var/log/nginx/access.log-nocache ]
 then
 	NOCACHE=$(awk '{ sum += $9 } END { printf "%.3f", sum/(1024^3) }'  /var/log/nginx/access.log-nocache)
 fi
 
-echo "MISS: $MISSES GiB"
+echo "MISS/EXPIRED: $MISSES GiB"
 echo "HIT/REVALIDATED: $HITS GiB"
 if [ -n "$NOCACHE" ]
 then
